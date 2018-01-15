3600cd92204d3ad708560db73f460e4406bd893e
VKisselev <vvladimir@my.bcit.ca>
2018-01-15 12:28:02 -0800

    Main assignment file
    Uploading barebones starting point for the new database

---
+++ Assignment01_KisselevV_AuE.sql
@@ -0,0 +1,10 @@
+-- Enabling ECHO and TAB
+SET ECHO ON
+SET TAB OFF
+
+-- SPOOL Location, may need to be adjusted depending on host
+SPOOL C:\Users\Vladimir\Documents\GitHub\RDS\RDS-Assignment-1\Assignment01_Spool_KisselevV_AuE.txt
+
+
+-- Disabling SPOOL to prevent data loss
+SPOOL OFF
